cfe5189aa2956718fa5a44a2a5d8b4fa1ff04c63
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2025-03-30 20:41:05 -0500

    allow host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: "dominion-taxidermy-wi-ec92e7c89fac.herokuapp.com"
+  }
 })
