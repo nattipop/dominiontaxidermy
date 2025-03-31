958e02f3f256e022bf787ef7df9a05890ddeb7d2
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2025-03-30 20:51:56 -0500

    mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css'
-import App from './src/App.jsx'
+import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
