51594aeeb5d55f77e573940161b0b7f267f9e606
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2025-03-30 20:42:35 -0500

    aksdfa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
   preview: {
     allowedHosts: "dominion-taxidermy-wi-ec92e7c89fac.herokuapp.com"
